df160e5f0ccd7e90a1b8b5095d5f3b86c388943e
Daniel P. Doyle <thedanieldoyle@outlook.com>
2019-11-02 20:17:39 +0000

    Introduced Context to encapsulate resolution of events, invoker and dispatcher.

---
+++ Source/EventDispatcher.EntityFrameworkCore/EventDispatcherContext.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace EventDispatcher.EntityFrameworkCore
+{
+    internal class EventDispatcherContext<TEvent>
+        where TEvent : class, IEvent
+    {
+        internal EventDispatcherContext(IEventDispatcher dispatcher, IEventDispatchInvoker<TEvent> invoker, IEnumerable<TEvent> events)
+        {
+            Dispatcher = dispatcher;
+            Events = events;
+            Invoker = invoker;
+        }
+
+        public IEventDispatcher Dispatcher { get; }
+
+        public IEnumerable<TEvent> Events { get; }
+
+        public IEventDispatchInvoker<TEvent> Invoker { get; }
+
+        public static EventDispatcherContext<TEvent> Create(DbContext context)
+        {
+            IEventDispatcher dispatcher = GetDispatcher(context);
+            IEnumerable<TEvent> events = GetEvents(context);
+            IEventDispatchInvoker<TEvent> invoker = GetInvoker(context);
+            return new EventDispatcherContext<TEvent>(dispatcher, invoker, events);
+        }
+
+        private static IEventDispatcher GetDispatcher(DbContext context)
+        {
+            return context.GetService<IEventDispatcher>();
+        }
+
+        private static IEnumerable<TEvent> GetEvents(DbContext context)
+        {
+            return context.ChangeTracker.Entries<TEvent>().Select(e => e.Entity);
+        }
+
+        private static IEventDispatchInvoker<TEvent> GetInvoker(DbContext context)
+        {
+            return context.GetService<IEventDispatchInvoker<TEvent>>();
+        }
+    }
+}
--- Source/EventDispatcher.EntityFrameworkCore/EventDispatcherDbCommandInterceptor.cs
+++ Source/EventDispatcher.EntityFrameworkCore/EventDispatcherDbCommandInterceptor.cs
@@ -1,57 +1,48 @@
-﻿using System.Collections.Generic;
-using System.Data.Common;
-using System.Linq;
+﻿using System.Data.Common;
 using System.Threading;
 using System.Threading.Tasks;
 using EventDispatcher;
+using EventDispatcher.EntityFrameworkCore;
 
 namespace Microsoft.EntityFrameworkCore.Diagnostics
 {
     public class EventDispatcherDbCommandInterceptor : DbCommandInterceptor
     {
-        private readonly IEventDispatcher dispatcher;
-        private readonly IEventDispatchInvoker<IDomainEvent> domainEventInvoker;
-        private readonly IEventDispatchInvoker<IIntegrationEvent> integrationEventInvoker;
 
-        public EventDispatcherDbCommandInterceptor(IEventDispatcher dispatcher, IEventDispatchInvoker<IDomainEvent> domainEventInvoker, IEventDispatchInvoker<IIntegrationEvent> integrationEventInvoker)
+        public EventDispatcherDbCommandInterceptor()
         {
-            this.dispatcher = dispatcher;
-            this.domainEventInvoker = domainEventInvoker;
-            this.integrationEventInvoker = integrationEventInvoker;
         }
 
         public override int NonQueryExecuted(DbCommand command, CommandExecutedEventData eventData, int result)
         {
             int returnCode = base.NonQueryExecuted(command, eventData, result);
-            this.dispatcher.Dispatch(GetEvents<IIntegrationEvent>(eventData.Context), integrationEventInvoker);
+            EventDispatcherContext<IIntegrationEvent> context = EventDispatcherContext<IIntegrationEvent>.Create(eventData.Context);
+            context.Dispatcher.Dispatch(context.Events, context.Invoker);
             return returnCode;
         }
 
         public override async Task<int> NonQueryExecutedAsync(DbCommand command, CommandExecutedEventData eventData, int result, CancellationToken cancellationToken = new CancellationToken())
         {
             int returnCode = await base.NonQueryExecutedAsync(command, eventData, result, cancellationToken);
-            await this.dispatcher.DispatchAsync(GetEvents<IIntegrationEvent>(eventData.Context), integrationEventInvoker, cancellationToken);
+            EventDispatcherContext<IIntegrationEvent> context = EventDispatcherContext<IIntegrationEvent>.Create(eventData.Context);
+            await context.Dispatcher.DispatchAsync(context.Events, context.Invoker, cancellationToken);
             return returnCode;
         }
 
         public override InterceptionResult<int> NonQueryExecuting(DbCommand command, CommandEventData eventData, InterceptionResult<int> result)
         {
-            this.dispatcher.Dispatch(GetEvents<IDomainEvent>(eventData.Context), domainEventInvoker);
+            EventDispatcherContext<IDomainEvent> context = EventDispatcherContext<IDomainEvent>.Create(eventData.Context);
+            context.Dispatcher.Dispatch(context.Events, context.Invoker);
             InterceptionResult<int> interceptionResult = base.NonQueryExecuting(command, eventData, result);
             return interceptionResult;
         }
 
         public override async Task<InterceptionResult<int>> NonQueryExecutingAsync(DbCommand command, CommandEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = new CancellationToken())
         {
-            IList<IDomainEvent> events = GetEvents<IDomainEvent>(eventData.Context).ToList();
-            await this.dispatcher.DispatchAsync(events, domainEventInvoker, cancellationToken);
+            EventDispatcherContext<IDomainEvent> context = EventDispatcherContext<IDomainEvent>.Create(eventData.Context);
+            await context.Dispatcher.DispatchAsync(context.Events, context.Invoker, cancellationToken);
             InterceptionResult<int> interceptionResult = await base.NonQueryExecutingAsync(command, eventData, result, cancellationToken);
             return interceptionResult;
-        }
-
-        private static IEnumerable<TEvent> GetEvents<TEvent>(DbContext context) where TEvent : class, IEvent
-        {
-            return context.ChangeTracker.Entries<TEvent>().Select(e => e.Entity);
         }
     }
 }
